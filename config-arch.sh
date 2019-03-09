a61e4c8aa46680d5d59f8dc07179c3183018973c
themoses <themoses@users.noreply.github.com>
2019-03-09 23:17:41 +0100

    Update config-arch.sh

--- config-arch.sh
+++ config-arch.sh
@@ -4,17 +4,17 @@
 # Part 1 can be found at install-arch.sh
 
 # Install tools
-pacman -Sy reflector --no-confirm
+pacman -Sy reflector --noconfirm
 reflector --verbose --country 'Germany' -l 200 -p https --sort rate --save /etc/pacman.d/mirrorlist
-pacman -Sy vim zsh grub git --no-confirm
+pacman -Sy vim zsh grub git --noconfirm
 
 # Set hostname
 echo lolwut-arch > /etc/hostname
 
 # Set language
 echo LANG=en_US.UTF-8 > /etc/locale.conf
-sed '/s/#en_US ISO-8859-1/en_US ISO-8859-1' /etc/locale.gen
-sed '/s/#en_US.UTF-8/en_US.UTF-8' /etc/locale.gen
+sed -i '/s/#en_US ISO-8859-1/en_US ISO-8859-1/g' /etc/locale.gen
+sed -i '/s/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
 locale-gen
 
 # Set keyboard binding
@@ -24,10 +24,9 @@ echo KEYMAP=de_CH-latin1 > /etc/vconsole.conf
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 
 # Enable multilib Support
-sed '/s/#[multilib]/[multilib]' /etc/pacman.conf
-sed '/s/#SigLevel = PackageRequired TrustedOnly/SigLevel = PackageRequired TrustedOnly' /etc/pacman.conf
-sed '/s/#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlist' /etc/pacman.conf
-pacman -Syu
+sed -i '/s/#[multilib]/[multilib]/g' /etc/pacman.conf
+sed -i '/s/#SigLevel = PackageRequired TrustedOnly/SigLevel = PackageRequired TrustedOnly/g' /etc/pacman.conf
+sed -i '/s/#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlist/g' /etc/pacman.conf
 
 # Generate initramfs
 mkinitcpio -p linux
@@ -38,17 +37,18 @@ grub-install /dev/sda1
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Adding user
-useradd -m -s /bin/zsh -U -g wheel, audio, video, power moses
+useradd -m -s /bin/zsh -U moses
+usermod -g wheel moses
  
 # Adding sudo access
-sed '/s/#%wheel  ALL=(ALL)       ALL/%wheel  ALL=(ALL)       ALL' /etc/sudoers
+sed -i '/s/#%wheel  ALL=(ALL)       ALL/%wheel  ALL=(ALL)       ALL/g' /etc/sudoers
 
 # Install tools
-pacman -S acpid dbus avahi cups cronie networkmanager --no-confirm
+pacman -S acpid dbus avahi cups cronie networkmanager --noconfirm
 systemctl enable acpid avahi-daemon cronie
 
 # Install Xorg
-pacman -S xorg-server xorg-xinit ttf-dejavu --no-confirm
+pacman -S xorg-server xorg-xinit ttf-dejavu --noconfirm
 
 # Set Keymapping for X Session
 echo > Section "InputClass" /etc/X11/xorg.conf.d/20-keyboard.conf
@@ -63,7 +63,7 @@ echo >>EndSection /etc/X11/xorg.conf.d/20-keyboard.conf
 #pacman -S nvidia --no-confirm
 
 # Install MATE
-pacman -S lightdm lightdm-gtk-greeter alsa pulseaudio-alsa mate network-manager-applet firefox
+pacman -S lightdm lightdm-gtk-greeter alsa pulseaudio-alsa mate network-manager-applet firefox --noconfirm
 
 systemctl enable lightdm NetworkManager
 
