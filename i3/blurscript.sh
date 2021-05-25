b3baadeaf452100268613b36cb9c6933f3b9f0d5
themoses <moritz_mechelke@hotmail.com>
2021-05-25 19:28:05 +0200

    Rework directory structure and update rofi call

--- i3/blurscript.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-scrot /home/moses/.config/i3/temp.png
-convert /home/moses/.config/i3/temp.png -blur "0x2" /home/moses/.config/i3/tempblur.png
-i3lock -i /home/moses/.config/i3/tempblur.png
-rm /home/moses/.config/i3/*.png
