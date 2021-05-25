e902376513f2ceadc4910da74abe696a07fd2492
themoses <moritz_mechelke@hotmail.com>
2021-05-25 20:12:32 +0200

    Add vim to bootstrap.sh

--- bash-setup/bootstrap.sh
+++ bash-setup/bootstrap.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+set -e
 
+source /etc/lsb-release
 
+install_vim(){
+    
+    case $DISTRIB_ID in
+        "Arch")
+            sudo pacman -Sy vim --noconfirm
+        ;;
+
+        "Ubuntu")
+            sudo apt install vim --yes
+        ;;
+    esac
+
+    cp vim/.vimrc ~/.vimrc
+}
+
+install_vim
