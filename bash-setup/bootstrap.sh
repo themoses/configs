736fd16fc48fddf558f5d4d2c689745613a52fe9
themoses <moritz_mechelke@hotmail.com>
2021-05-26 09:33:22 +0200

    Add rofi and shortcut setup

--- bash-setup/bootstrap.sh
+++ bash-setup/bootstrap.sh
@@ -3,9 +3,9 @@ set -e
 
 source /etc/lsb-release
 
-install_vim(){
+setup_vim(){
     
-    case $DISTRIB_ID in
+    case "$DISTRIB_ID" in
         "Arch")
             sudo pacman -Sy vim --noconfirm
         ;;
@@ -13,9 +13,42 @@ install_vim(){
         "Ubuntu")
             sudo apt install vim --yes
         ;;
+
+        *)
+            # do nothing
+        ;;
     esac
 
     cp vim/.vimrc ~/.vimrc
 }
 
-install_vim
+setup_rofi(){
+    
+    case "$DISTRIB_ID" in
+        "Arch")
+            sudo pacman -Sy rofi dconf --noconfirm
+        ;;
+
+        "Ubuntu")
+            sudo apt install rofi dconf-tools --yes
+        ;;
+    esac
+
+    mkdir -p ~/.config/rofi/config
+    cp rofi/config ~/.config/rofi/config
+
+# set up key bindings for rofi
+    case "$XDG_SESSION_DESKTOP" in
+        "mate" | "gnome-xorg")
+            dconf write /org/mate/desktop/keybindings/custom0/name "'rofi'"
+            dconf write /org/mate/desktop/keybindings/custom0/action "'rofi -show drun -display-drun \"\"'"
+            dconf write /org/mate/desktop/keybindings/custom0/binding "'<Mod4>space'"
+
+        *)
+            # do nothing
+        ;;
+    esac
+}
+
+setup_vim
+setup_rofi
